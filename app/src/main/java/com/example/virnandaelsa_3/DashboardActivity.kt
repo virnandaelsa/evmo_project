5387d4563a8e9a4270b12894813de9a6b9b458c0
SirRHO <rizkinurrohman4424@gmail.com>
2024-09-30 09:58:43 +0700

    dash

---
+++ app/src/main/java/com/example/virnandaelsa_3/DashboardActivity.kt
@@ -0,0 +1,11 @@
+package com.example.virnandaelsa_3
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.registrasi)
+    }
+}
