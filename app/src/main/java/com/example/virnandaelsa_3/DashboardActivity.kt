d218906b6ed97ff9383cbfc1dbfe959459bde410
SirRHO <rizkinurrohman4424@gmail.com>
2024-10-07 10:26:52 +0700

    frag2

--- app/src/main/java/com/example/virnandaelsa_3/DashboardActivity.kt
+++ app/src/main/java/com/example/virnandaelsa_3/DashboardActivity.kt
@@ -6,6 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 class DashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.frag_pemesanan)
+        setContentView(R.layout.frag_profile)
     }
 }
