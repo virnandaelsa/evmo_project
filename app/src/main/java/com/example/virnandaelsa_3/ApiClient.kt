b1068ccc4ba1a6b3f0aad920db4008ca9ffc9c4e
dwinarwastu <admin@dwinarwastu.com>
2024-11-24 19:03:14 +0700

    foom-strawberry

--- app/src/main/java/com/example/virnandaelsa_3/ApiClient.kt
+++ app/src/main/java/com/example/virnandaelsa_3/ApiClient.kt
@@ -7,7 +7,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
-    private const val BASE_URL = "http://192.168.1.5:8000/api/"
+    private const val BASE_URL = "http://192.168.1.11:8000/api/"
 
     val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
